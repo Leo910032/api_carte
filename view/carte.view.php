e9fe86fcf28c6c7783e6f9f03c87ecab687b14c8
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2025-01-13 11:23:32 +0100

    new file:   public/design/img/iconeCarte.jpg 	modified:   public/design/styleCarte.css 	new file:   public/region 	modified:   view/carte.view.php

--- view/carte.view.php
+++ view/carte.view.php
@@ -87,6 +87,7 @@
             </div>
         </div>
         <div id="carteDroite">
+            <img src="public/design/img/iconeCarte.jpg" class="iconeCarte" id="Limousin">
             <img src="public/design/img/carte.png" alt="Carte régionale">
         </div>
     </section>
