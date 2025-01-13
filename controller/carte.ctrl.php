6a0acfcacf45443f0ffbdb3c15f084cd1c160719
Tristan Petit <tristan.petit@etu.univ-grenoble-alpes.fr>
2025-01-13 10:07:08 +0100

    modified:   controller/carte.ctrl.php 	deleted:    controller/carte.view.php 	renamed:    view/img/carte.png -> public/design/img/carte.png 	renamed:    view/img/commentaire.png -> public/design/img/commentaire.png 	renamed:    view/img/grotte.jpeg -> public/design/img/grotte.jpeg 	renamed:    view/img/logo.png -> public/design/img/logo.png 	renamed:    view/img/map.png -> public/design/img/map.png 	renamed:    view/img/panier.png -> public/design/img/panier.png 	renamed:    view/img/poubelle.png -> public/design/img/poubelle.png 	renamed:    view/img/site1.png -> public/design/img/site1.png 	renamed:    view/img/site2.png -> public/design/img/site2.png 	renamed:    view/img/site3.png -> public/design/img/site3.png 	renamed:    view/img/site4.png -> public/design/img/site4.png 	renamed:    view/img/site5.png -> public/design/img/site5.png 	renamed:    view/img/site6.png -> public/design/img/site6.png 	renamed:    view/img/temple.jpeg -> public/design/img/temple.jpeg 	renamed:    view/style.css -> public/design/style.css 	renamed:    view/styleCarte.css -> public/design/styleCarte.css 	modified:   view/carte.view.php

--- controller/carte.ctrl.php
+++ controller/carte.ctrl.php
@@ -2,6 +2,5 @@
 require_once(__DIR__.'/../framework/view.fw.php');
 
 
-
 $view = new View();
 $view->display('carte');
--- view/carte.view.php
+++ view/carte.view.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./styleCarte.css">
+    <link rel="stylesheet" href="../public/design/style.css" type="text/css">
+    <link rel="stylesheet" href="../public/design/styleCarte.css" type="text/css">
     <!-- Pour les logos menu hamburger-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />-->
     <title>Site archéologique</title>
 </head>
 <body>
@@ -32,7 +32,7 @@
             <h2>Région : Rhone-Alpes</h2>
             <div id="carteScroll">
                 <a href="" class="contenu">
-                    <img id="imgSite" src="img/site5.png" alt="Site archéologique">
+                    <img id="imgSite" src="../public/design/img/site5.png" alt="Site archéologique">
                     <div class="contenuTexte">
                         <p id="nom">Nom</p>                        
                         <p id="prix">Prix</p>
@@ -45,7 +45,7 @@
                 <!-- Un "a" est un site -->
 
                 <a href="" class="contenu">
-                    <img id="imgSite" src="img/site5.png" alt="Site archéologique">
+                    <img id="imgSite" src="../public/design/img/site5.png" alt="Site archéologique">
                     <div class="contenuTexte">
                         <p id="nom">Nom</p>                        
                         <p id="prix">Prix</p>
@@ -55,7 +55,7 @@
                     </div>
                 </a>
                 <a href="" class="contenu">
-                    <img id="imgSite" src="img/site5.png" alt="Site archéologique">
+                    <img id="imgSite" src="../public/design/img/site5.png" alt="Site archéologique">
                     <div class="contenuTexte">
                         <p id="nom">Nom</p>                        
                         <p id="prix">Prix</p>
@@ -65,7 +65,7 @@
                     </div>
                 </a>
                 <a href="" class="contenu">
-                    <img id="imgSite" src="img/site5.png" alt="Site archéologique">
+                    <img id="imgSite" src="../public/design/img/site5.png" alt="Site archéologique">
                     <div class="contenuTexte">
                         <p id="nom">Nom</p>                        
                         <p id="prix">Prix</p>
@@ -75,7 +75,7 @@
                     </div>
                 </a>
                 <a href="" class="contenu">
-                    <img id="imgSite" src="img/site5.png" alt="Site archéologique">
+                    <img id="imgSite" src="../public/design/img/site5.png" alt="Site archéologique">
                     <div class="contenuTexte">
                         <p id="nom">Nom</p>                        
                         <p id="prix">Prix</p>
